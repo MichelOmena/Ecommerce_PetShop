c7a953f78999bb4668941331a057ae9728b88a02
Michel Omena <micheltenoriodeomena@hotmail.com>
2024-09-21 13:22:18 +0100

    class update and test

--- public/index.php
+++ public/index.php
@@ -1,11 +1,18 @@
 <?php
 // abrir a sessao
+
+use core\class\Database;
+
 session_start();
 
 //Carregar o config 
 require_once('../config.php');
 
+require_once('../vendor/autoload.php');
 
+$a = new Database();
+
+echo 'OK';
 
 /*
 Carregar o config

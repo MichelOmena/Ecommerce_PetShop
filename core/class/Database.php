897354fc794243bc1866c1596885c7b1cb7c9cf8
Michel Omena <micheltenoriodeomena@hotmail.com>
2024-09-21 13:48:51 +0100

    initial db connection

--- core/class/Database.php
+++ core/class/Database.php
@@ -3,10 +3,25 @@
 namespace core\class;
 
 class Database{
-    /* gestao de base de dados */
 
-    public function __construct()
-    {
-        echo 'Base de dados!';
-    }
+    private $connection;
+
+    private function on
+
+    
 }
+
+ /*
+    CRUD
+    Create   - INSERT
+    READ     - SELECT
+    Update   - UPDATE
+    Delete   -DELETE
+
+/*
+define('MYSQL_SERVER',    'localhost');
+define('MYSQL_DATABASE',  'php_store');
+define('MYSQL_USER',      'user_php_store');
+define('MYSQL_PASS',      '');
+define('MYSQL_CHARSET',   'utf8');
+*/
--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
-
-use core\class\Database;
-use core\class\functions;
-
 // abrir a sessao
 session_start();
 
@@ -13,19 +9,4 @@ require_once('../config.php');
 //Carrega todas as classes do projeto
 require_once('../vendor/autoload.php');
 
-$a = new Database();
-$b = new functions();
-
-$b->test();
-
-echo 'OK';
-
-/*
-Carregar o config
-Carregar classes
-Carregar o sistema de rotas
-      - mostrar loja
-      - mostrar carinho
-      - mostrar backoffice, etc
- */
 ?>
